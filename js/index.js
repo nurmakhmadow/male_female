ba2fea1afa9dca432d0e6479433337eb0e371329
nurmakhmadow <nurmakhmadow@mail.ru>
2023-06-15 17:30:03 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,47 @@
+let students = [
+    {
+        name: "Dilshod",
+        age: 23,
+        gender: "male"
+    },
+    {
+        name: "Emin",
+        age: 18,
+        gender: "female"
+    },
+    {
+        name: "Aziz",
+        age: 16,
+        gender: "male"
+    },
+    {
+        name: "Jafar",
+        age: 12,
+        gender: "female"
+    },
+    {
+        name: "Mexrshod",
+        age: 27,
+        gender: "female"
+    },
+    {
+        name: "Samir",
+        age: 6,
+        gender: "male"
+    }
+]
+
+let male = 0
+let female = 0
+
+students.filter(baby => {
+    if(baby.gender === 'male'){
+        male++
+    } else if(baby.gender === 'female') {
+        female++
+    }
+}) 
+
+console.log(female);
+
+console.log(male);
